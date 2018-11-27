d94193816c7f190e44c3b5fc9360221c38a2576c
YOUR NAME <YOUR@EMAIL.com>
2018-11-26 23:27:24 -0800

    added probelm 8

---
+++ problem_8.py
@@ -0,0 +1,16 @@
+'''
+A unival tree (which stands for "universal value") 
+is a tree where all nodes under it have the same value.
+
+Given the root to a binary tree, count the number of unival subtrees.
+'''
+class Node:
+    def __init__(self,v,left=None,right=None):
+        self.v = v
+        self.left = left
+        self.right = right
+
+def count_unival(root):
+    pass
+
+

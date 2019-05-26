66c995182635a7e4c9f916ecba42c5f5134f38d8
Paul Rodriguez <paul@Pauls-MacBook-Pro.local>
2019-05-25 22:06:57 -0700

    some more tests for problem 186 and added complexities for time and space

--- problem_186.py
+++ problem_186.py
@@ -2,7 +2,14 @@
 Given an array of positive integers, 
 divide the array into two subsets such that the difference between the sum of the subsets is as small as possible.
 '''
+'''
+time complexity: O(nlogn)
+amount of time spent sorting. we iterate the array once
 
+space complexity: O(1)
+only four variables that are not the answers are used
+
+'''
 def divideArray(A):
 	s1 = []
 	s2 = []
@@ -28,4 +35,6 @@ print(divideArray([5,10,15,20,25]))
 print(divideArray([10,100,300]))
 print(divideArray([1,2,3,4,5,6,7,8,9]))
 print(divideArray([1,10,100,300,900]))
-		
+print(divideArray([3,1,4,2,2,1]))
+print(divideArray([40,80,100]))
+	
